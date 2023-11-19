f966ec9c2a0d8b8496e2653b6621eb61ec0a0393
Srushti Sonavane <96761322+Srushti-S@users.noreply.github.com>
2023-11-19 22:28:27 +0530

    Huffman Encoding using a greedy strategy

--- DAA/A2.py
+++ DAA/A2.py
@@ -1,63 +1,42 @@
-import heapq  # Import the heapq module for the priority queue
-from collections import defaultdict  # Import defaultdict to count character frequencies
+import heapq
 
-# Function to build the Huffman tree
-def buildHuffmanTree(text):
-    char_freq = defaultdict(int)  # Create a dictionary to store character frequencies
+class node:
+    def __init__(self,freq,symbol,left=None,right=None):
+        self.freq=freq # frequency of symbol
+        self.symbol=symbol # symbol name (character)
+        self.left=left # node left of current node
+        self.right=right # node right of current node
+        self.huff= '' # # tree direction (0/1)
 
-    # Calculate the frequency of each character in the input text
-    for char in text:
-        char_freq[char] += 1
+    def __lt__(self,nxt): # Check if curr frequency less than next nodes freq
+        return self.freq<nxt.freq
 
-    # Create a priority queue to store the Huffman nodes
-    heap = [[weight, [char, ""]] for char, weight in char_freq.items()]  # Create a list of nodes (initially, one for each character)
-    heapq.heapify(heap)  # Convert the list into a min-heap
+def printnodes(node, val=''):
+    newval = val + str(node.huff)
 
-    while len(heap) > 1:
-        lo = heapq.heappop(heap)  # Pop the node with the lowest weight
-        hi = heapq.heappop(heap)  # Pop the node with the second lowest weight
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]  # Append '0' to the codes of characters in the left branch
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]  # Append '1' to the codes of characters in the right branch
-        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])  # Combine the nodes and push the result back into the heap
+    if node.left:
+        printnodes(node.left, newval)
+    if node.right:
+        printnodes(node.right, newval)
 
-    return sorted(heap[0][1:], key=lambda p: (len(p[-1]), p))  # Return the sorted list of character-code pairs
-
-# Function to encode text using Huffman codes
-def huffmanEncode(text):
-    huffman_tree = buildHuffmanTree(text)  # Build the Huffman tree
-    huffman_codes = dict(huffman_tree)  # Convert the tree into a dictionary for easy lookup
-    encoded_text = ''.join(huffman_codes[char] for char in text)  # Encode the input text using Huffman codes
-    return encoded_text  # Return the encoded text
-
-# Function to decode Huffman-encoded text
-def huffmanDecode(encoded_text, huffman_tree):
-    decoded_text = ''
-    while encoded_text:
-        for char, code in huffman_tree:
-            if encoded_text.startswith(code):  # Check if the encoded text starts with a Huffman code
-                decoded_text += char  # Append the corresponding character to the decoded text
-                encoded_text = encoded_text[len(code):]  # Remove the matched code from the beginning of the encoded text
-                break
-    return decoded_text  # Return the decoded text
+    if not node.left and not node.right:
+        print("{} -> {}".format(node.symbol, newval))
 
 if __name__ == "__main__":
-    input_text = "simple example for huffman encoding"
-    
-    # Encode the input text using Huffman codes
-    encoded_text = huffmanEncode(input_text)
-    
-    # Print Huffman codes in the format "letter: Huffman code"
-    huffman_tree = buildHuffmanTree(input_text)
-    huffman_codes = dict(huffman_tree)
-    print("\nLetter : HuffmanCode\n")
-    for char, code in sorted(huffman_codes.items()):
-        print(f"{char} : {code}")
+    chars = ['a', 'b', 'c', 'd', 'e', 'f']
+    freq = [5, 9, 12, 13, 16, 45]
+    nodes = []
 
-    # Decode the encoded text
-    decoded_text = huffmanDecode(encoded_text, huffman_tree)
-    
-    print("\nOriginal Text:", input_text)
-    print("\nEncoded Text:", encoded_text)
-    print("\nDecoded Text:", decoded_text)
+    for i in range(len(chars)):
+        heapq.heappush(nodes, node(freq[i], chars[i]))
+
+    while len(nodes) > 1:
+        left = heapq.heappop(nodes)
+        right = heapq.heappop(nodes)
+
+        left.huff = '0'
+        right.huff = '1'
+        newnode = node(left.freq + right.freq, left.symbol + right.symbol, left, right)
+        heapq.heappush(nodes, newnode)
+
+    printnodes(nodes[0])
