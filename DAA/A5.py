8b37fab16fbe81439a7cc8766deaacc2be763e53
Srushti Sonavane <96761322+Srushti-S@users.noreply.github.com>
2023-11-19 22:22:24 +0530

    Quick Sort

--- DAA/A5.py
+++ DAA/A5.py
@@ -1,6 +1,5 @@
 import random
 
-# Deterministic QuickSort
 def deterministic_quick_sort(arr):
   if len(arr) <= 1:
     return arr
@@ -11,7 +10,7 @@ def deterministic_quick_sort(arr):
 
   for item in arr[1:]:
     global comparisons
-    comparisons += 1  # Increment the comparison count
+    comparisons += 1  
     if item < pivot:
       left.append(item)
     else:
@@ -19,7 +18,7 @@ def deterministic_quick_sort(arr):
 
   return deterministic_quick_sort(left) + [pivot] + deterministic_quick_sort(right)
 
-# Randomized QuickSort
+
 def randomized_quick_sort(arr):
   if len(arr) <= 1:
     return arr
@@ -32,7 +31,7 @@ def randomized_quick_sort(arr):
 
   for idx, item in enumerate(arr):
     global comparisons
-    comparisons += 1  # Increment the comparison count
+    comparisons += 1  
     if idx == pivot_idx:
       continue
     if item < pivot:
@@ -42,7 +41,6 @@ def randomized_quick_sort(arr):
 
   return randomized_quick_sort(left) + [pivot] + randomized_quick_sort(right)
 
-# Function to count the number of comparisons in quicksort
 def count_comparisons(arr, sorting_algorithm):
   global comparisons
   comparisons = 0
@@ -51,16 +49,14 @@ def count_comparisons(arr, sorting_algorithm):
   return comparisons, sorted_arr
 
 if __name__ == "__main__":
-  # Input list to be sorted
+
   input_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 
-  # Analyze deterministic quicksort
   deterministic_comparisons, deterministic_sorted = count_comparisons(input_list.copy(), deterministic_quick_sort)
   print("Deterministic QuickSort:")
   print("Comparisons:", deterministic_comparisons)
   print("Sorted Array:", deterministic_sorted)
 
-  # Analyze randomized quicksort
   randomized_comparisons, randomized_sorted = count_comparisons(input_list.copy(), randomized_quick_sort)
   print("\nRandomized QuickSort:")
   print("Comparisons:", randomized_comparisons)
