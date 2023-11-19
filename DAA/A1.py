3f920d2f91ac750846e18482983f532b9cf6ac6b
Srushti Sonavane <96761322+Srushti-S@users.noreply.github.com>
2023-11-19 22:37:21 +0530

    Fibonacci Numbers

--- DAA/A1.py
+++ DAA/A1.py
@@ -1,15 +1,11 @@
 def recursive_fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n<=1:
+        return n
     return recursive_fibo(n - 1) + recursive_fibo(n - 2)
 
 def iterative_fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n<=1:
+        return n
     fibo1 = 0
     fibo2 = 1
     for i in range(2, n + 1):
@@ -21,19 +17,20 @@ def iterative_fibo(n):
 def main():
     flag = 1
     while flag == 1:
-        print("Choose a method to calculate Fibonacci:")
         print("1. Recursive")
         print("2. Iterative")
-        choice = int(input())
+        choice = int(input("Choose a method to calculate Fibonacci: "))
         
         if choice == 1:
             n = int(input("Enter the value of n: "))
             result = recursive_fibo(n)
             print(f"Fibonacci({n}) = {result}")
+            
         elif choice == 2:
             n = int(input("Enter the value of n: "))
             result = iterative_fibo(n)
             print(f"Fibonacci({n}) = {result}")
+            
         else:
             print("Invalid choice.")
         flag = int(input("Do you want to continue? (1/0): "))
